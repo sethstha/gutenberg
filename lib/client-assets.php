2491f7fad08860f2dae8f38af224ffd6b4a4b7e1
iseulde <wp@iseulde.com>
2018-08-29 13:01:30 +0200

    Restore splitting on line breaks in list transform

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -387,7 +387,7 @@ function gutenberg_register_scripts_and_styles() {
 			'wp-keycodes',
 			'wp-polyfill-ecmascript',
 			'wp-url',
-			'wp-blocks',
+			'wp-rich-text-structure',
 		),
 		filemtime( gutenberg_dir_path() . 'build/components/index.js' ),
 		true
